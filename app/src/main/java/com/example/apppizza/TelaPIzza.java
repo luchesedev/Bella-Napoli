c8f8f27ded8fa5184707d40aa76e843e14fbeed9
Pedro <pedroluchese@gmail.com>
2026-03-26 20:52:54 -0300

    tela pizzas quase pronta

--- app/src/main/java/com/example/apppizza/TelaPIzza.java
+++ app/src/main/java/com/example/apppizza/TelaPIzza.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,6 +13,7 @@ public class TelaPIzza extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_pizza);
